92aa25e7489dee18767e9328f93ff267291942ea
Imam Usmani <imam.usmani@haag-streit.com>
2022-12-08 08:51:51 +0100

    install epubmerge

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ then
 	path=`dirname $0`
 	apt install -y calibre
 	cp "`dirname $0`/news_loader_recipes" $HOME/.config/calibre/
+	wget https://www.mobileread.com/forums/attachment.php?attachmentid=128768&d=1663081894
+	calibre-customize -a EpubMerge.zip
 else
 	echo "please run this script as root. create one if necessary with the command:
 	sudo passwd root"
