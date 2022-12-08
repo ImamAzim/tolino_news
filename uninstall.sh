40e30247d673d41f0ef758a7e0e214ebbaadae3a
Imam Usmani <imam.usmani@haag-streit.com>
2022-12-08 10:12:28 +0100

    uninstall new stuff

--- uninstall.sh
+++ uninstall.sh
@@ -5,6 +5,12 @@ if [ $UID = 0 ]
 then
 	path=`dirname $0`
 	rm -rf $HOME/.config/calibre/news_loader_recipes
+	calibre-customize -r EpubMerge
+	rmdir /mnt/ereader
+	rm /etc/systemd/system/mnt-ereader.mount
+	systemctl daemon-reload
+	rm /etc/udev/rules.d/50-automountusb.rules
+	udevadm control --reload
 	echo " you can now remove the source files in `dirname $0`"
 	echo "you can remove calibre if you want"
 else
