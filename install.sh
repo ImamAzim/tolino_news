2cc92e796e261e3f30f20e5fa59f533a7c699967
Imam Usmani <imam@e.email>
2022-12-10 22:27:09 +0100

    cp rules install

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ then
 	systemctl daemon-reload
 
 	cp "`dirname $0`/50-load_news.rules" /etc/udev/rules.d/
+	cp "`dirname $0`/49-load_news.rules" /etc/udev/rules.d/
 	udevadm control --reload
 
 	cp "`dirname $0`/load_news.service" /etc/systemd/system/
