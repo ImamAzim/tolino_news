a69d49fca1585854dba49ff48049299bab760cf6
Imam Usmani <imam@e.email>
2022-12-10 18:09:28 +0100

    uninstall rule

--- uninstall.sh
+++ uninstall.sh
@@ -4,14 +4,11 @@
 if [ $UID = 0 ]
 then
 	path=`dirname $0`
-	#rm -rf $HOME/.config/calibre/news_loader_recipes
 	calibre-customize -r EpubMerge
-	#rmdir /mnt/ereader
-	#rm /etc/systemd/system/mnt-ereader.mount
 	rm /etc/systemd/system/load_news.service
 	systemctl daemon-reload
-	#rm /etc/udev/rules.d/50-automountusb.rules
-	#udevadm control --reload
+	rm /etc/udev/rules.d/50-load_news.rules
+	udevadm control --reload
 	rm /usr/local/bin/load_news.py
 	echo " you can now remove the source files in `dirname $0`"
 	echo "you can remove calibre if you want"
