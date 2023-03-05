8dabe1e4c8bb489ed901d94e65c04d47232f7ce0
ImamAzim <imam@e.email>
2023-03-05 18:09:02 +0100

    uninstall script

--- dev_uninstall.sh
+++ dev_uninstall.sh
@@ -5,6 +5,7 @@ if [ $UID = 0 ]
 then
 	path=`dirname $0`
 	calibre-customize -r EpubMerge
+	rm -rf /usr/local/bin/news_loader
 	echo " you can now remove the source files in `dirname $0`"
 	echo "you can remove calibre if you want"
 	echo "you can remove you recipe and credentials in $HOME/.config/calibre/news_loader_recipes"
