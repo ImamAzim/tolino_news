1769a551beec9dabe703328ffb222887e4d3c8ea
ImamAzim <imam@e.email>
2023-03-05 18:12:00 +0100

    update script

--- dev_update.sh
+++ dev_update.sh
@@ -4,7 +4,9 @@
 if [ $UID = 0 ]
 then
 	path=`dirname $0`
-
+	git -C $path pull
+	source /usr/local/bin/news_loader/bin/activate
+	pip install $path
 else
 	echo "please run this script as root. create one if necessary with the command:
 	sudo passwd root"
