1796ee46c485fb4a526dcde3ac5ed6138b92889a
Imam Usmani <imam@e.email>
2023-11-04 20:54:48 +0100

    add tests

---
+++ tests/test_models.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+
+"""
+test models
+"""
+
+import unittest
+
+
+from news_loader.models import NewsCreator
+
+
+class TestNewsCreator(unittest.TestCase):
+
+    """all test concerning NewsCreator. """
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
