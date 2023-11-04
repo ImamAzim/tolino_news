da7e5a0dcb96571039236424a0bebefb85a1c29a
Imam Usmani <imam@e.email>
2023-11-04 20:52:28 +0100

    create class news creator

---
+++ src/news_loader/models.py
@@ -0,0 +1,13 @@
+
+
+class NewsCreator(object):
+
+    """contains tools to fetch news, merge to an epub and upload it"""
+
+    def __init__(self):
+        """TODO: to be defined. """
+        pass
+
+
+if __name__ == '__maine__':
+    pass
