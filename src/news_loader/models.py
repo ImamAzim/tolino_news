1bf990975a85358adad9a0854eb6711fbc7349c8
Imam Usmani <imam@e.email>
2023-11-05 11:48:35 +0100

    newsloaderconfig class

--- src/news_loader/models.py
+++ src/news_loader/models.py
@@ -32,6 +32,21 @@ class NewsCreator(object):
             shutil.copy(CONFIG_FP, path)
 
 
+class NewsLoaderConfiguration(object):
+
+    """class to set configuration of the news loader app"""
+
+    def __init__(self):
+        """initiate and create dict config and path to file"""
+        self._config_dict = dict()
+
+        directory = os.path.join(xdg.XDG_CONFIG_HOME, 'news_loader')
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        self.config_fp = os.path.join(directory, 'config.toml')
+
+
 if __name__ == '__main__':
     print(os.path.dirname(__file__))
     pass
