c715a53eb8c01c95ed7a6b89253af949204507c0
Imam Usmani <imam@e.email>
2023-11-05 22:04:16 +0100

    app and views models

---
+++ src/news_loader/apps.py
@@ -0,0 +1,33 @@
+from pyxperiments.controllers.base import ExampleController
+from pyxperiments.views.base import ExampleView
+
+
+def main():
+    pass
+
+
+def example():
+    app = ExampleApp()
+    app.start()
+
+
+class ExampleApp(object):
+
+    """example app"""
+
+    def __init__(self):
+        """
+        construct the app
+        """
+
+        view = ExampleView()
+        controller = ExampleController(view)
+        view.set_controller(controller)
+        self._view = view
+
+    def start(self):
+        self._view.start()
+
+
+if __name__ == '__main__':
+    main()
