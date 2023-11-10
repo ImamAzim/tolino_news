94384551e0ef2265a23467e91a5750b142e5970e
Imam Usmani <imam@e.email>
2023-11-10 22:29:00 +0100

    update install

--- dev_install.sh
+++ dev_install.sh
@@ -1,27 +1,15 @@
 #!/bin/bash
 
 
-if [ $UID = 0 ]
-then
-	path=`dirname $0`
-	apt install -y calibre
-	mkdir -p $HOME/.config/calibre/custom_recipes
-	mkdir /etc/news_loader
-	cp -n "$path/news_loader/config.toml" /etc/news_loader
+path=`dirname $0`
+wget --output-document=EpubMerge.zip https://www.mobileread.com/forums/attachment.php?attachmentid=128768&d=1663081894
+rm wget-log
+calibre-customize -a EpubMerge.zip
+rm EpubMerge.zip
 
-	wget --output-document=EpubMerge.zip https://www.mobileread.com/forums/attachment.php?attachmentid=128768&d=1663081894
-	rm wget-log
-	calibre-customize -a EpubMerge.zip
-	rm EpubMerge.zip
+apt install -y python3-venv
+python3 -m venv /usr/local/lib/news_loader
+source /usr/local/lib/news_loader/bin/activate
+pip install $path
+ln -fs /usr/local/lib/news_loader/bin/news_loader /usr/local/bin
 
-	apt install -y python3-venv
-	python3 -m venv /usr/local/bin/news_loader
-	source /usr/local/bin/news_loader/bin/activate
-	pip install $path
-
-	cp  "$path/cron_daily_news" /etc/cron.d/
-
-else
-	echo "please run this script as root. create one if necessary with the command:
-	sudo passwd root"
-fi
