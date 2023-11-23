5f15655e809c57034b7f87eb652dc8b4681b7e8f
Imam Usmani <imam@e.email>
2023-11-23 14:48:47 +0100

    install as sudo

--- dev_install.sh
+++ dev_install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
 
 path=`dirname $0`
 wget --output-document=EpubMerge.zip https://www.mobileread.com/forums/attachment.php?attachmentid=128768&d=1663081894
 rm wget-log
-calibre-customize -a EpubMerge.zip
+sudo -u $real_user calibre-customize -a EpubMerge.zip
 rm EpubMerge.zip
 
 apt install -y python3-venv
