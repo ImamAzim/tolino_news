591dd8abdd5ad63e7933726d7c1b5d62c56b015e
Imam Usmani <imam@e.email>
2023-11-23 21:34:39 +0100

    no plugin install on dev script install

--- dev_install.sh
+++ dev_install.sh
@@ -13,13 +13,6 @@ fi
 
 path=`dirname $0`
 
-url="https://www.mobileread.com/forums/attachment.php?s=0280306cc395ea788c3be735f52d91a0&attachmentid=128768&d=1689785083"
-plugin_file="attachment.php?s=0280306cc395ea788c3be735f52d91a0&attachmentid=128768&d=1689785083"
-base_url="https://www.mobileread.com/forums/"
-wget "$base_url$plugin_file"
-sudo -u $real_user calibre-customize -a $plugin_file
-rm $plugin_file
-
 apt install -y python3-venv
 python3 -m venv /usr/local/lib/news_loader
 source /usr/local/lib/news_loader/bin/activate
