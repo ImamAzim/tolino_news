ed74f63d87070104c324c10550e5f99e9d28b567
Imam Usmani <imam@e.email>
2023-11-23 20:42:26 +0100

    fix wget url

--- dev_install.sh
+++ dev_install.sh
@@ -12,15 +12,18 @@ else
 fi
 
 path=`dirname $0`
-sudo -u $real_user wget --output-document=/tmp/epubmerge.zip https://www.mobileread.com/forums/attachment.php?attachmentid=128768&d=1663081894
-sudo -u $real_user calibre-customize -a /tmp/epubmerge.zip
-#rm "$path/EpubMerge.zip"
-#sudo -u $real_user rm wget-log
 
-#apt install -y python3-venv
-#python3 -m venv /usr/local/lib/news_loader
-#source /usr/local/lib/news_loader/bin/activate
-#pip install $path
-#ln -fs /usr/local/lib/news_loader/bin/news_loader /usr/local/bin
-#ln -fs /usr/local/lib/news_loader/bin/news_loader_run /usr/local/bin
+url="https://www.mobileread.com/forums/attachment.php?s=0280306cc395ea788c3be735f52d91a0&attachmentid=128768&d=1689785083"
+plugin_file="attachment.php?s=0280306cc395ea788c3be735f52d91a0&attachmentid=128768&d=1689785083"
+base_url="https://www.mobileread.com/forums/"
+wget "$base_url$plugin_file"
+sudo -u $real_user calibre-customize -a $plugin_file
+rm $plugin_file
+
+apt install -y python3-venv
+python3 -m venv /usr/local/lib/news_loader
+source /usr/local/lib/news_loader/bin/activate
+pip install $path
+ln -fs /usr/local/lib/news_loader/bin/news_loader /usr/local/bin
+ln -fs /usr/local/lib/news_loader/bin/news_loader_run /usr/local/bin
 
