a684ed0d3a795d6d9310a7bdb4e8a2c5d6a38bd4
ImamAzim <imam@e.email>
2024-01-04 13:07:14 +0100

    rename in uninstall

--- dev_uninstall.sh
+++ dev_uninstall.sh
@@ -12,9 +12,9 @@ else
 fi
 
 path=`dirname $0`
-rm -rf /usr/local/lib/news_loader
-rm -f /usr/local/bin/news_loader
-rm -f /usr/local/bin/news_loader_run
+rm -rf /usr/local/lib/tolino_news
+rm -f /usr/local/bin/tolino_news
+rm -f /usr/local/bin/tolino_news_run
 echo "if you did not remove the crontab job before, you have to do it manually"
 echo " you can now remove the source files in $path"
 echo "you can remove calibre or epubmerge plugin if you want"
