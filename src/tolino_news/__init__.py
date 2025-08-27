722ae17c2722239301291d87fd354b0dc80d5440
Imam Usmani <imam@e.email>
2025-08-27 11:50:32 +0200

    fix name exist_ok

--- src/tolino_news/__init__.py
+++ src/tolino_news/__init__.py
@@ -6,7 +6,7 @@ import xdg_base_dirs
 
 APP_NAME = 'tolino_news'
 log_folder = xdg_base_dirs.xdg_state_home() / APP_NAME
-log_folder.mkdir(exists_ok=True)
+log_folder.mkdir(exist_ok=True)
 LOG_FP = log_folder / 'log'
 exec_fn = 'tolino_news_run'
 RUNJOB_FP = Path('/usr', 'local', 'bin', exec_fn)  # created at install
