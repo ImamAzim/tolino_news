9498b690c2c3b584b9e7653118ee1025ee7625ee
Imam Usmani <imam@e.email>
2025-08-28 12:31:28 +0200

    check clalibre installation def

--- src/tolino_news/models/epub_creators.py
+++ src/tolino_news/models/epub_creators.py
@@ -6,17 +6,35 @@ from tolino_news import PLUGIN_FP
 from tolino_news.models.interfaces import BaseEpubCreator
 
 
+class EpubCreatorError(Exception):
+    pass
+
+
 class EpubCreator(BaseEpubCreator):
+
+    def _check_calibre_installation(self):
+        """
+        :returns: TODO
+
+        """
+        pass
+
+    def _check_epubmergeplugin_installation(self):
+        """
+        :returns: TODO
+
+        """
+        self._check_calibre_installation()
 
     def download_news(self,
                       recipe_fp: Path,
                       username: None | str = None,
                       password: None | str = None,
                       ) -> Path:
-        pass
+        self._check_calibre_installation()
 
     def merge_epubs(self, epub_fps: list[Path]) -> Path:
-        pass
+        self._check_epubmergeplugin_installation()
 
     def clean_cache_folder(self):
         pass
