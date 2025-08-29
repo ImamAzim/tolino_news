8d43e62461c416a994d11346f1543930c6efd7d9
Imam Usmani <imam@e.email>
2025-08-29 22:14:02 +0200

    def view and update

--- src/tolino_news/views.py
+++ src/tolino_news/views.py
@@ -1,9 +1,10 @@
 import getpass
 
 
-from tolino_news.models import NewsLoaderConfiguration
-from tolino_news.jobs import run_news_loader, register_device
-from tolino_news.jobs import unregister_device
+from tolino_news.models.epub_creators import EpubCreator
+from tolino_news.models.configurators import Configurator, ConfiguratorError
+from tolino_news.models.cloud_connectors import cloud_connectors
+from tolino_news.jobs import run_news_loader
 
 
 class NewsLoaderMenu(object):
@@ -15,17 +16,15 @@ class NewsLoaderMenu(object):
                 '0': 'install epubmerge plugin (mandatory before first use)',
                 '1': 'add configuration file',
                 '2': 'delete configuration file',
-                '3': 'register device (necessary to do it once before'
-                'first use)',
-                '4': 'unregister device',
-                '5': 'test the tolino news now',
-                '6': 'add a crontab job to tolino news',
-                '7': 'delete crontab job',
-                '8': 'show configuration file',
+                '3': 'test the tolino news now',
+                '4': 'add a crontab job to tolino news',
+                '5': 'delete crontab job',
+                '6': 'show configuration file',
                 'q': 'quit',
                 }
         self._running = True
-        self.config = NewsLoaderConfiguration()
+        self._configurator = Configurator()
+        self._epub_creator = EpubCreator()
 
     def start(self):
         self._print_welcome()
@@ -52,7 +51,7 @@ class NewsLoaderMenu(object):
         """install epubmerge plugin
 
         """
-        self.config.install_epubmerge_plugin()
+        self._epub_creator.install_epubmerge_plugin()
         print('===')
 
     def case_1(self):
@@ -105,33 +104,17 @@ class NewsLoaderMenu(object):
         """delete configuration file
 
         """
-        self.config.delete_config()
+        self._configurator.delete_config()
         print('===')
 
     def case_3(self):
-        """register device
-
-        """
-        msg = register_device()
-        print(msg)
-        print('===')
-
-    def case_4(self):
-        """unregister device
-
-        """
-        msg = unregister_device()
-        print(msg)
-        print('===')
-
-    def case_5(self):
         """test the tolino news now
 
         """
         run_news_loader()
         print('===')
 
-    def case_6(self):
+    def case_4(self):
         """add crontab job
 
         """
@@ -148,31 +131,27 @@ class NewsLoaderMenu(object):
                 print('invalid minutes')
             else:
                 try:
-                    self.config.add_in_crontab(hour, minute)
-                except FileExistsError:
-                    print('there is already such cron job. '
-                          'please delete it first.')
-
+                    self._configurator.add_in_crontab(hour, minute)
+                except ConfiguratorError as e:
+                    print(e)
         print('===')
 
-    def case_7(self):
+    def case_5(self):
         """delete crontab job
 
         """
-        self.config.del_crontab()
+        self._configurator.del_crontab()
         print('cron job deleted. Tolino News will not run daily anymore')
         print('===')
 
-    def case_8(self):
+    def case_6(self):
         """show config file
 
         """
         try:
-            config_dict = self.config.load_config()
-        except FileNotFoundError:
-            print('file not found! did you create a config files?')
-        else:
-            print(config_dict)
+            print(self._configurator)
+        except ConfiguratorError as e:
+            print(e)
         print('===')
 
     def case_q(self):
