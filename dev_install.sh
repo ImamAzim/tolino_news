3b386ba3c785f329aac295f9fb1e23c3a1c6fa98
Imam Usmani <imam@e.email>
2026-03-09 21:34:30 +0100

    install calibre

--- dev_install.sh
+++ dev_install.sh
@@ -20,6 +20,7 @@ apt install -y libopengl0
 apt install -y xvfb
 apt install -y chromium
 apt install -y chromium-driver
+apt install -y calibre
 python3 -m venv /usr/local/lib/tolino_news
 source /usr/local/lib/tolino_news/bin/activate
 pip install $path
@@ -27,5 +28,4 @@ ln -fs /usr/local/lib/tolino_news/bin/tolino_news /usr/local/bin
 ln -fs /usr/local/lib/tolino_news/bin/token_update /usr/local/bin
 ln -fs /usr/local/lib/tolino_news/bin/tolino_news_run /usr/local/bin
 echo "install complete!"
-echo "you need to install now calibre if it is not present"
-echo "then you can run tolino_news from any user and configure"
+echo "you can run tolino_news from any user and configure"
